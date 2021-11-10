64e72d819fb86e66eccea1279057d10c7d0c1b06
qasim-wpbrigade <87133962+qasim-wpbrigade@users.noreply.github.com>
2021-11-10 14:24:13 +0500

    Complete custom select

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React,{useState} from "react";
 import "./app.css";
-import CodeEditor from './components/codeEditor'
+// import CodeEditor from './components/codeEditor';
+import CustomSelect from './components/customSelect';
 
 function App() {
+  const [data, setdata] = useState([{label:'item 1',value:'item1'},{label:'item 2',value:'item2'}])
   return (
     <div className='mightyContainer'>
-      <CodeEditor />
+      <CustomSelect onChange={(e) => console.log(e)} data={data} />
     </div>
   );
 }
--- src/components/customSelect/index.js
+++ src/components/customSelect/index.js
@@ -1,40 +1,55 @@
 /* eslint-disable no-unused-vars */
-import React,{useRef, useState, useEffect} from 'react';
-import './customSelect.css';
-import {slideToggle, slideUp} from '../slideToggle';
+import React, { useRef, useState, useEffect } from "react";
+import "./customSelect.css";
+import { slideToggle, slideUp } from "../slideToggle";
+import PropTypes from "prop-types";
 
-const Index = () => {
+const Index = ({ data, onChange, placeholder }) => {
     const mdr = useRef(null);
     const msb = useRef(null);
-    const [value, setvalue] = useState('');
-    const [label, setlabel] = useState('select one')
-    const [data, setdata] = useState([{label:'item 1',value:'item1'},{label:'item 2',value:'item2'}])
-    const changeValue = (v,l) => {
-        setvalue(v);
+    const [label, setlabel] = useState(placeholder ?? "select one");
+
+    const changeValue = (v, l) => {
         setlabel(l);
-        slideUp(mdr.current)
-    }
+        slideUp(mdr.current);
+        if(onChange) return onChange(v);
+    };
 
     useEffect(() => {
-        document.addEventListener('click', (e) => {
+        document.addEventListener("click", (e) => {
             if (!msb.current.contains(e.target)) {
-                slideUp(mdr.current)
+                slideUp(mdr.current);
             }
-        })
-    },[])
+        });
+    }, []);
     return (
-        <div className='mightySelect' style={{maxWidth:'400px'}}>
-            <button ref={msb} onClick={() => slideToggle(mdr.current)}><span>{label}</span></button>
+        <div className="mightySelect" style={{ maxWidth: "400px" }}>
+            <button ref={msb} onClick={() => slideToggle(mdr.current)}>
+                <span>{label}</span>
+            </button>
             <ul ref={mdr} className="mightySelectDropdown">
-            <li onClick={() => changeValue('','select one')}>select one</li>
-                {
-                    data.map((row, i) => (
-                        <li onClick={() => changeValue(row.value,row.label)} key={i}>{row.label}</li>
-                    ))
-                }
+                <li
+                    onClick={() => changeValue("", placeholder ?? "select one")}
+                >
+                    {placeholder ?? "select one"}
+                </li>
+                {data?.map((row, i) => (
+                    <li
+                        onClick={() => changeValue(row?.value, row?.label)}
+                        key={i}
+                    >
+                        {row.label}
+                    </li>
+                ))}
             </ul>
         </div>
-    )
-}
+    );
+};
 
-export default Index
+Index.propTypes = {
+    data: PropTypes.array,
+    onChange: PropTypes.func,
+    placeholder: PropTypes.string,
+};
+
+export default Index;
